e2492bf9fc6ac193f72f43b4841c2d15eef050e1
Yosef Stifanos <yosefstifanos30@gmail.com>
2024-11-19 13:42:38 -0600

    added JSON file saving

--- command-line-data-curation/cli_data_entry.py
+++ command-line-data-curation/cli_data_entry.py
@@ -1,5 +1,6 @@
 from rich.console import Console
 from rich.table import Table
+import json
 
 """ All information about the players and are from the following sources: (https://www.spotrac.com) """
 
@@ -42,56 +43,42 @@ def print_players():
         for field, value in player.items():
             console.print(f"[bright_green]{field}[/bright_green]: {value}")
 
-print_players()
+def save_players_to_file(filename):
+    with open(filename, 'w') as file:
+        json.dump(favorite_players, file, indent=4)
 
-def addPlayer():
+def add_player():
     while True:
         console.print("What is the name of your favorite player?", style="bright_green")
         player = input("Player: ")
-
-        console.print("What team does your favorite player play for?", style="bright_green")
+        console.print("What team does the player play for?", style="bright_green")
         team = input("Team: ")
+        console.print("What is the player's age?", style="bright_green")
+        age = int(input("Age: "))
+        console.print("What is the player's salary?", style="bright_green")
+        salary = int(input("Salary: "))
 
-        console.print("What is the age of your favorite player?", style="bright_green")
-        age = input("Age: ")
+        favorite_players.append({
+            "Player": player,
+            "Team": team,
+            "Age": age,
+            "Salary": salary
+        })
 
-        console.print("What is the salary of your favorite player? (you can look this up on the Internet)", style="bright_green")
-        salary = input("Salary: ")
+        console.print(f"[bright_green]{player} has been added to your favorite players list.[/bright_green]")
 
-        favorite_player = {"Player": player, "Team": team, "Age": age, "Salary": salary}
-
-        console.print('Does this look correct?', style="bright_green")
-        for field, value in favorite_player.items():
-            console.print(f"[magenta]{field}[/magenta]: {value}")
-
-        response = input("Y/N: ")
-        if response == "Y":
-            favorite_players.append(favorite_player)
+        console.print("Do you want to add another player? (yes/no)", style="bright_green")
+        if input().lower() != 'yes':
             break
-        else:
-            console.print("Let's try again.", style="bright_green")
 
-def prompt():
-    while True:
-        console.print("Would you like to add another player? Make sure you view your player(s) before leaving the program! Please respond with Yes/No/View", style="bright_green")
-        response = input("Yes/View/No: ")
+# Print players
+print_players()
 
-        if response.lower() == "yes":
-            addPlayer()
-        elif response.lower() == "view":
-            print_players()
-        elif response.lower() == "no":
-            console.print('Have a great day!!!', style="bright_green")
-            break
-        else:
-            console.print("You chose nothing! Make sure you choose Yes, No, or View", style="bright_green")
+# Save players to a local file
+save_players_to_file('favorite_players.json')
 
-console.print("\nWould you care to share your favorite players? Please respond Y/N (To simply put it; YOU ARE FORCED TO ADD A PLAYER HAHAHAHAHAH :D )", style="bright_green")
-y_n = input("Y/N: ")
+# Add new players
+add_player()
 
-while y_n.lower() != "y":
-    console.print("What did I tell you!", style="bright_green")
-    y_n = input("Y/N: ")
-
-addPlayer()
-prompt()
+# Save updated players to a local file
+save_players_to_file('favorite_players.json')
